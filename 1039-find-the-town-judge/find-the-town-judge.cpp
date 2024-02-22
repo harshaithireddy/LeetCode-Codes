4bb9aa9fbc81e066e1fd53fdcadf3bfa0935c99e
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2024-02-22 18:10:34 +0530

    Time: 183 ms (15.95%) | Memory: 82.2 MB (5.08%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
         if(n == 1) return 1;
-        unordered_map<int, vector<int>> mpp;
+        unordered_map<int, int> mpp;
         for(auto it : trust) {
-            mpp[it[1]].push_back(it[0]);
+            mpp[it[1]]++;
         }
         
         unordered_map<int, int> mpp2;
@@ -12,14 +12,8 @@ public:
             mpp2[it[0]]++;
         }
 
-        // if(mpp.size() > 1) return -1;
         for(auto it : mpp) {
-            vector<int> x = it.second;
-            cout << it.first << "->";
-            for(auto i : x) cout << i << " ";
-        }
-        for(auto it : mpp) {
-            if(it.second.size() == n-1 && mpp2.find(it.first) == mpp2.end()) return it.first; 
+            if(it.second == n-1 && mpp2.find(it.first) == mpp2.end()) return it.first; 
         }
         return -1;
     }
