313166f642cf8d5fa51c05d702164c685804d930
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2024-02-24 21:45:54 +0530

    Time: 7 ms (96.58%) | Memory: 33.6 MB (5.15%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    Solution() {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+    }
     vector<vector<int>> findMatrix(vector<int>& nums) {
         vector<vector<int>> V;
         
