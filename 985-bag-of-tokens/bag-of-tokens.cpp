ceb41f796b68be803e2e64eacd11d6217aeb8e47
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2024-03-04 10:25:15 +0530

    Time: 9 ms (18.89%) | Memory: 13.1 MB (55.08%) - LeetSync

--- 985-bag-of-tokens/bag-of-tokens.cpp
+++ 985-bag-of-tokens/bag-of-tokens.cpp
@@ -5,22 +5,17 @@ public:
 
         sort(tokens.begin(), tokens.end());
         if(tokens[0] > power) return 0;
-        
+
         int i = 0;
         int j = tokens.size() - 1;
         
         int score = 0;
         int maxi = -1;
         while(i <= j) {
-            // if(score > 0 && i == j) {
-            //     maxi = max(maxi, score);
-            //     break;
-            // }
             if(tokens[i] <= power) {
                 score += 1;
                 power -= tokens[i];
                 i++;
-                //if(i == j) break;
             }
             else if(score > 0 && power + tokens[j] > tokens[i]) {
                 score -= 1;
@@ -32,7 +27,6 @@ public:
                 j--;
             }
             maxi = max(maxi, score);
-            //if(tokens[i] > power || score == 0) break;
         }
         return maxi;
     }
