2f7b9f05edf4514ba13a4f57f14900cd8910b8b5
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2024-03-07 22:06:49 +0530

    Time: 202 ms (38.57%) | Memory: 56 MB (71.43%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.cpp
+++ 1972-rotating-the-box/rotating-the-box.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     vector<vector<char>> rotateMatrix(vector<vector<char>>& matrix) {
-    int m = matrix.size();
-    int n = matrix[0].size();
+        int m = matrix.size();
+        int n = matrix[0].size();
 
-    vector<vector<char>> transposed(n, vector<char>(m));
-    for(int i = 0; i < n; ++i) {
-        for(int j = 0; j < m; ++j) {
-            transposed[i][j] = matrix[j][i];
+        vector<vector<char>> transposed(n, vector<char>(m));
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                transposed[i][j] = matrix[j][i];
+            }
         }
-    }
 
-    for(int i = 0; i < n; ++i) {
-        reverse(transposed[i].begin(), transposed[i].end());
+        for(int i = 0; i < n; i++) {
+            reverse(transposed[i].begin(), transposed[i].end());
+        }
+        return transposed;
     }
-    return transposed;
-}
 
     vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
         for(auto &it : box) {
@@ -50,11 +50,6 @@ public:
                 }
             }
         }
-        // for(auto it : box) {
-        //     vector<char> x = it;
-        //     for(auto i : x) cout << i << " ";
-        // }
-        // cout << endl;
 
         vector<vector<char>> res = rotateMatrix(box);
         return res;
