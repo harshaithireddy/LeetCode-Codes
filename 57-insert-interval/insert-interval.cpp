5ff705e9f4969ae4b0fd19189e7230f18cd57495
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2024-03-17 09:09:54 +0530

    Time: 0 ms (100.00%) | Memory: 20.2 MB (94.78%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        ios_base::sync_with_stdio(false);
         vector<vector<int>> res;
 
         int i = 0;
