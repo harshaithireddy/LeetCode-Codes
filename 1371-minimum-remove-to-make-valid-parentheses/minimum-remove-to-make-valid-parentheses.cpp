42c452320b88c8311aacebd88eeca9ebbf541181
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2024-04-06 12:18:06 +0530

    Time: 23 ms (44.88%) | Memory: 13.7 MB (22.61%) - LeetSync

---
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        stack<int> st;
+        unordered_set<int> set;
+
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] == '(') st.push(i);
+            else if(s[i] == ')') {
+                if(!st.empty()) st.pop();
+                else set.insert(i);
+            }
+        }
+        while(!st.empty()) {
+            set.insert(st.top());
+            st.pop();
+        }
+
+        string res = "";
+        for(int i = 0; i < s.size(); i++) {
+            if(!set.contains(i)) res.push_back(s[i]); 
+        }
+        return res;
+    }
+};
