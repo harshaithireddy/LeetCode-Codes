40da0b1042b26a6034cd0d4cb00fec7247ba0c50
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2024-04-09 19:20:26 +0530

    Time: 6 ms (13.43%) | Memory: 10.1 MB (25.58%) - LeetSync

---
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        queue<int> q;
+        for(int i = 0; i < tickets.size(); i++) {
+            q.push(i);
+        }
+
+        int res = 0;
+        while(!q.empty()) {
+            res++;
+
+            int x = q.front();
+            q.pop();
+            tickets[x]--;
+            
+            if(k == x && tickets[x] == 0) break;
+            if(tickets[x] != 0) q.push(x);
+        }
+        return res;
+    }
+};
