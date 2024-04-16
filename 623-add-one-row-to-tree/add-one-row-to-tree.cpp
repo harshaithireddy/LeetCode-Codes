810320ee9c171c617927ddd838ef83efa8f07d68
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2024-04-16 20:11:15 +0530

    Time: 15 ms (40.93%) | Memory: 24.4 MB (49.35%) - LeetSync

---
+++ 623-add-one-row-to-tree/add-one-row-to-tree.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void dfs(TreeNode* root, int val, int depth, int currDepth) {
+        if(root == NULL) return;
+
+        if(currDepth < depth - 1) {
+            dfs(root->left, val, depth, currDepth + 1);
+            dfs(root->right, val, depth, currDepth + 1);
+        }
+
+        if(currDepth == depth - 1) {
+            TreeNode* newLeft = new TreeNode(val);
+            TreeNode* newRight = new TreeNode(val);
+            TreeNode* left = root->left;
+            TreeNode* right = root->right;
+
+            root->left = newLeft;
+            root->right = newRight;
+
+            newLeft->left = left;
+            newRight->right = right;
+        }
+    }
+    TreeNode* addOneRow(TreeNode* root, int val, int depth) {
+        if(depth == 1) {
+            TreeNode* newChild = root;
+            TreeNode* newRoot = new TreeNode(val);
+            newRoot->left = newChild;
+            return newRoot;
+        }
+
+        dfs(root, val, depth, 1);
+        return root;
+    }
+};
