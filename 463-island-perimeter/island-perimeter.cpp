e36d25c61ad4a0dbabcfb2d4f93f41e14799c2ff
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2024-04-18 20:49:13 +0530

    Time: 61 ms (91.00%) | Memory: 100.8 MB (48.27%) - LeetSync

--- 463-island-perimeter/island-perimeter.cpp
+++ 463-island-perimeter/island-perimeter.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int islandPerimeter(vector<vector<int>>& grid) {
+        ios_base::sync_with_stdio(false);
         int up, down, left, right, res = 0;
         for(int i = 0; i < grid.size(); i++) {
             for(int j = 0; j < grid[0].size(); j++) {
