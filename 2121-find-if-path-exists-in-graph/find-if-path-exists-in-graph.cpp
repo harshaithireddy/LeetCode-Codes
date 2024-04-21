cade7b6e9574eacf467e80299c073787551d14e0
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2024-04-21 19:53:14 +0530

    Time: 2330 ms (5.01%) | Memory: 236.7 MB (19.73%) - LeetSync

---
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void dfs(int node, vector<bool>& vis, vector<vector<int>>& grid) {
+        if(vis[node] == true) return;
+        vis[node] = true;
+
+        for(auto it : grid[node]) {
+            dfs(it, vis, grid);
+        }
+    }
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        vector<bool> vis(n, false);
+        vector<vector<int>> grid(n);
+        for(auto it : edges) {
+            int u = it[0];
+            int v = it[1];
+            grid[u].push_back(v);
+            grid[v].push_back(u);
+        }
+        dfs(source, vis, grid);
+        return vis[destination];
+    }
+};
