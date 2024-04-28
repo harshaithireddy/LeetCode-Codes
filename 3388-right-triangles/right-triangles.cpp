86d30e3fe680f8ca63332511a29cf3a04a6f4fdf
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2024-04-28 20:49:18 +0530

    Time: 259 ms (25.00%) | Memory: 113.2 MB (25.00%) - LeetSync

---
+++ 3388-right-triangles/right-triangles.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    long long numberOfRightTriangles(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<int> rowCnt(n, 0), colCnt(m, 0);
+
+        for(int i = 0; i < n; i++) {
+            int cnt = 0;
+            for(int j = 0; j < m; j++) {
+                if(grid[i][j] == 1) cnt++;
+            }
+            rowCnt[i] = cnt;
+        }
+
+        for(int j = 0; j < m; j++) {
+            int cnt = 0;
+            for(int i = 0; i < n; i++) {
+                if(grid[i][j] == 1) cnt++;
+            }
+            colCnt[j] = cnt;
+        }
+
+        long long res = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(grid[i][j] == 1) res += ((rowCnt[i] - 1) * (colCnt[j] - 1));
+            }
+        }
+        return res;
+    }
+};
