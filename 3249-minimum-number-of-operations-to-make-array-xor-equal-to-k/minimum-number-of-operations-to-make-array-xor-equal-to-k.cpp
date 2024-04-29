4255dc80ba5162b4887abed2c9fb4e5b93ff521b
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2024-04-29 06:46:44 +0530

    Time: 75 ms (98.08%) | Memory: 91.9 MB (6.81%) - LeetSync

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
         int res = 0;
-        for(int bit = 0; bit <= 20; bit++) {
+        for (int bit = 0; bit <= 20; bit++) {
             int xorVal = 0;
-            for(auto it : nums) {
+            for (auto it : nums) {
                 int bitVal = (1 << bit) & it;
                 xorVal = xorVal ^ bitVal;
             }
             int kBitVal = (1 << bit) & k;
-            if(kBitVal != xorVal) res++;
+            if (kBitVal != xorVal) res++;
         }
         return res;
     }
