33e894577bc322bc6a818dba2852a0a5014e8253
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2024-05-02 20:27:32 +0530

    Time: 21 ms (44.04%) | Memory: 26.7 MB (7.36%) - LeetSync

--- 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.cpp
+++ 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int findMaxK(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
         int res = -1;
         unordered_set<int> st(nums.begin(), nums.end());
         for(auto it : st) {
