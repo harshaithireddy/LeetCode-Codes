27b7e09bd4fdb7a586c360c6e8cbe1a9b4f2524a
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2024-05-14 20:11:39 +0530

    Time: 104 ms (80.00%) | Memory: 9 MB (94.87%) - LeetSync

---
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int gold = 0;
+    void traverse(int i, int j, int sum, vector<vector<int>>& grid) {
+        int row = grid.size();
+        int col = grid[0].size();
+        
+        if(i < 0 || i >= row || j < 0 || j >= col || grid[i][j] == 0) return;
+
+        int temp = grid[i][j];
+        sum += grid[i][j];
+
+        gold = max(gold, sum);
+        grid[i][j] = 0;
+
+        traverse(i+1, j, sum, grid);
+        traverse(i-1, j, sum, grid);
+        traverse(i, j+1, sum, grid);
+        traverse(i, j-1, sum, grid);
+
+        grid[i][j] = temp;
+    }
+    int getMaximumGold(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(grid[i][j] != 0) traverse(i, j, 0, grid);
+            }
+        }
+        return gold;
+    }
+};
