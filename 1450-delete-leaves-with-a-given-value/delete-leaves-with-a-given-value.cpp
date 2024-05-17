836f9b613f258ed8eef39acb00d65ca537f9b13b
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2024-05-17 19:47:38 +0530

    Time: 18 ms (9.51%) | Memory: 21.2 MB (83.50%) - LeetSync

---
+++ 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isLeaf(TreeNode* node) {
+        if(node == NULL) return false;
+        if(node->left == NULL && node->right == NULL) return true;
+        return false;
+    }
+    void traverse(TreeNode* root, int target) {
+        if(root == NULL) return;
+        traverse(root->left, target);
+        traverse(root->right, target);
+
+        if(isLeaf(root->left) && root->left->val == target) {
+            root->left = NULL;
+        }
+        if(isLeaf(root->right) && root->right->val == target) {
+            root->right = NULL;
+        }
+    }
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        traverse(root, target);
+        if(root->left == NULL && root->right == NULL && root->val == target) {
+            return NULL;
+        }
+        return root;
+    }
+};
