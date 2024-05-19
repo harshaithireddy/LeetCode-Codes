b5d5b5ca998685a64f8f17b57d8a0fbdfee7deaf
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2024-05-19 20:00:31 +0530

    Time: 197 ms (50.22%) | Memory: 125.6 MB (88.89%) - LeetSync

---
+++ 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
+        long long sum = 0;
+        int cnt = 0;
+        int mini = INT_MAX;
+
+        for(auto it : nums) {
+            if((it ^ k) > it) {
+                sum += it ^ k;
+                mini = min(mini, (it ^ k) - it);
+                cnt++;
+            }
+            else {
+                sum += it;
+                mini = min(mini, it - (it ^ k));
+            }
+        }
+        if(cnt % 2 == 0) return sum;
+        return sum - mini;
+    }
+};
