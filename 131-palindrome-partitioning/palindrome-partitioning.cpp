516b14a963cf233f432b0888872c0d8c1836db55
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2024-05-22 19:11:09 +0530

    Time: 100 ms (46.94%) | Memory: 57 MB (67.35%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isPalindrome(string s, int start, int end) {
+        while(start <= end) {
+            if(s[start++] != s[end--]) return false;
+        }
+        return true;
+    }
+
+    void dfs(int i, string& s, vector<string>& path, vector<vector<string>>& res) {
+        if(i == s.length()) {
+            res.push_back(path);
+            return;
+        }
+        for(int end = i; end < s.length(); end++) {
+            if(isPalindrome(s, i, end)) {
+                path.push_back(s.substr(i, end - i + 1));
+                dfs(end + 1, s, path, res);
+                path.pop_back();
+            }
+        }
+    }
+
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> res;
+        if(s.empty()) return res;
+
+        vector<string> path;
+        dfs(0, s, path, res);
+
+        return res;
+    }
+};
