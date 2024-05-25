f9dc27884ee6238e4feeee42026eb1a3a1c83dcc
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2024-05-25 23:28:42 +0530

    Time: 3 ms (34.00%) | Memory: 8.5 MB (98.68%) - LeetSync

---
+++ 140-word-break-ii/word-break-ii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<string> res;
+    void solve(int i, string &sentence, string &s, unordered_map<string, bool> &doesExist) {
+        if(i >= s.size()) {
+            res.push_back(sentence);
+            return;
+        }
+
+        for (int j = i + 1; j <= i + 10  &&  j <= s.size(); j++) {
+            string word = s.substr(i, j - i);
+            if(doesExist.find(word) != doesExist.end()) {
+                if(sentence.size() == 0) {
+                    sentence = word;
+                } else {
+                    sentence += " " + word;
+                }
+                solve(j, sentence, s, doesExist);
+                int x = 0;
+                while(x < word.size()) {
+                    x++;
+                    sentence.pop_back();
+                }
+
+                if(sentence.size() > 0) {
+                    sentence.pop_back();
+                }
+            }
+        }
+    }
+
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        string sentence = "";
+        unordered_map<string, bool> doesExist;
+        for(auto it : wordDict) {
+            doesExist[it] = true;
+        }
+
+        res.clear();
+        solve(0, sentence, s, doesExist);
+        return res;
+    }
+};
