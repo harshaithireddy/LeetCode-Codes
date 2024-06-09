c74b2bf7dd639b948e0bf7102197b1c68fdb5844
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2024-06-09 20:19:59 +0530

    Time: 27 ms (94.15%) | Memory: 35.2 MB (18.10%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.cpp
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
         int sum = 0;
         int cnt = 0;
         unordered_map<int, int> mpp;
