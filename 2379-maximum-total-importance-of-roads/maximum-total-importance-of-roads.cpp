536ab654ca9e86771a786c4b1e6cd9c9d4843471
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2024-06-28 19:23:12 +0530

    Time: 290 ms (64.48%) | Memory: 132.7 MB (57.93%) - LeetSync

---
+++ 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    long long maximumImportance(int n, vector<vector<int>>& roads) {
+        vector<int> deg(n, 0);
+        for(auto it : roads) {
+            deg[it[0]]++;
+            deg[it[1]]++;
+        }
+        sort(deg.begin(), deg.end());
+
+        long long val = 1;
+        long long res = 0;
+        for(auto it : deg) {
+            res += val * it;
+            val++;
+        }
+        return res;
+    }
+};
