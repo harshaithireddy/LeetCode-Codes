f954bc1db72cc0be360397a0d614c77d02f15af5
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2024-07-06 20:42:03 +0530

    Time: 3 ms (16.54%) | Memory: 7.2 MB (30.58%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.cpp
+++ 2645-pass-the-pillow/pass-the-pillow.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int passThePillow(int n, int time) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
         int rounds = time / (n-1);
         int x = time % (n-1);
 
