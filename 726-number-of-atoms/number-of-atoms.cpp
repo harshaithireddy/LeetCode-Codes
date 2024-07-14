77ace4259fb200c55b68dd9374cd78d6ac87029b
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2024-07-14 20:24:07 +0530

    Time: 5 ms (30.88%) | Memory: 9.1 MB (69.54%) - LeetSync

---
+++ 726-number-of-atoms/number-of-atoms.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    string countOfAtoms(string formula) {
+        int n = formula.size();
+        int i, j, k;
+        
+        vector<int> mulFactor(n, 1);
+        int mul = 1;
+        stack<int> st;
+
+        for(i = n-1; i >= 0; i--) {
+            char c = formula[i];
+            if(c == ')') {
+                string num = "";
+                for(j = i+1; j < n; j++) {
+                    c = formula[j];
+                    if(!isdigit(c)) break;
+                    num += c;
+                }
+
+                int digit = (num == "") ? 1 : stoi(num);
+                mul = mul * digit;
+                st.push(digit);
+            }
+            else if(c == '(') {
+                int digit = st.top();
+                st.pop();
+                mul = mul / digit;
+            }
+            mulFactor[i] = mul;
+        }
+
+        map<string, int> mpp;
+        for(int i = 0; i < n; i++) {
+            char c = formula[i];
+            if(isupper(c)) {
+                string ele = "";
+                ele += c;
+
+                for(j = i+1; j < n; j++) {
+                    c = formula[j];
+                    if(!islower(c)) break;
+                    ele += c;
+                }
+
+                string num = "";
+                for(k = j; k < n; k++) {
+                    c = formula[k];
+                    if(!isdigit(c)) break;
+                    num += c;
+                }
+                int dig = (num == "") ? 1 : stoi(num);
+
+                mpp[ele] += mulFactor[i] * dig;
+                i = k - 1;
+            }
+        }
+
+        string res = "";
+        for(auto it : mpp) {
+            res += it.first;
+            if(it.second > 1) res += to_string(it.second);
+        }
+        return res;
+    }
+};
