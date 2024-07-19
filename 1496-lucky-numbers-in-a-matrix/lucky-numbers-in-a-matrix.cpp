b2b5ea41b9fc33f09bcd445dba948b6a258d89e4
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2024-07-19 14:58:15 +0530

    Time: 15 ms (64.89%) | Memory: 15.2 MB (5.19%) - LeetSync

--- 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.cpp
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.cpp
@@ -8,13 +8,13 @@ public:
         int n = matrix[0].size();
         vector<int> res;
         
-        unordered_set<int> st;
+        unordered_map<int, bool> mpp;
         for(int i = 0; i < m; i++) {
             int mini = matrix[i][0];
             for(int j = 1; j < n; j++) {
                 mini = min(mini, matrix[i][j]);
             }
-            st.insert(mini);
+            mpp[mini] = true;
         }
 
         for(int i = 0; i < n; i++) {
@@ -22,7 +22,7 @@ public:
             for(int j = 1; j < m; j++) {
                 maxi = max(maxi, matrix[j][i]);
             }
-            if(st.find(maxi) != st.end()) res.push_back(maxi);
+            if(mpp[maxi] == true) res.push_back(maxi);
         }
         return res;
     }
