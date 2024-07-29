c9d1430499db913a7f5e19176f57575ecf842851
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2024-07-29 19:55:31 +0530

    Time: 28 ms (78.08%) | Memory: 11.7 MB (51.74%) - LeetSync

---
+++ 1511-count-number-of-teams/count-number-of-teams.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numTeams(vector<int>& rating) {
+        int res = 0;
+        int n = rating.size();
+
+        for(int i = 1; i < n - 1; i++) {
+            int leftSmaller = 0, leftLarger = 0;
+            int rightSmaller = 0, rightLarger = 0;
+
+            for(int j = 0; j < i; j++) {
+                if(rating[j] < rating[i]) leftSmaller++;
+                else if(rating[j] > rating[i]) leftLarger++;
+            }
+
+            for(int j = i + 1; j < n; j++) {
+                if(rating[j] < rating[i]) rightSmaller++;
+                else if(rating[j] > rating[i]) rightLarger++;
+            }
+
+            res += leftSmaller * rightLarger + leftLarger * rightSmaller;
+        }
+
+        return res;
+    }
+};
