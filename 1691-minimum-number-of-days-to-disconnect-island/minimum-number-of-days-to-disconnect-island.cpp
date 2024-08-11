d7ce5ee7093790cc6d19e9f64985815855864515
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2024-08-11 19:04:33 +0530

    Time: 489 ms (7.47%) | Memory: 100.5 MB (9.77%) - LeetSync

---
+++ 1691-minimum-number-of-days-to-disconnect-island/minimum-number-of-days-to-disconnect-island.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    void dfs(int r, int c, vector<vector<bool>>& vis, vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        if(r < 0 || r >= rows || c < 0 || c >= cols || grid[r][c] == 0 || vis[r][c]) return;
+        
+        vis[r][c] = true;
+        vector<pair<int, int>> dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        
+        for(auto dir : dirs) {
+            int nr = r + dir.first;
+            int nc = c + dir.second;
+            dfs(nr, nc, vis, grid);
+        }
+    }
+
+    int countIslands(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        vector<vector<bool>> vis(rows, vector<bool>(cols, false));
+        int count = 0;
+        
+        for(int r = 0; r < rows; r++) {
+            for(int c = 0; c < cols; c++) {
+                if(grid[r][c] == 1 && !vis[r][c]) {
+                    dfs(r, c, vis, grid);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    int minDays(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+
+        if(countIslands(grid) != 1) return 0;
+
+        for(int r = 0; r < rows; r++) {
+            for(int c = 0; c < cols; c++) {
+                if(grid[r][c] == 0) continue;
+                
+                grid[r][c] = 0;
+                if(countIslands(grid) != 1) return 1;
+                grid[r][c] = 1;
+            }
+        }
+        return 2;
+    }
+};
