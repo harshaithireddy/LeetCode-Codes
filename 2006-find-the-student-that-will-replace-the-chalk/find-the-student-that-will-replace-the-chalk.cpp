7bade5a75f0c0472b3626d06db6cb93b47d08301
Ithireddy Harsha Vardhan <119839919+harshaithireddy@users.noreply.github.com>
2024-09-02 17:56:23 +0530

    Time: 54 ms (96.69%) | Memory: 77 MB (18.08%) - LeetSync

--- 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.cpp
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    int speedup = []{ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); return 0;}();
     int chalkReplacer(vector<int>& chalk, int k) {
         long long sum = 0;
         for(int i = 0; i < chalk.size(); i++) {
