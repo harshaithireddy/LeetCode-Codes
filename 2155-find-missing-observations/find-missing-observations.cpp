d61c799b9bcac2f00d58afafed322abd31b61482
Ithireddy Harsha Vardhan <119839919+harshaithireddy@users.noreply.github.com>
2024-09-05 19:10:43 +0530

    Time: 98 ms (57.11%) | Memory: 126.5 MB (8.58%) - LeetSync

--- 2155-find-missing-observations/find-missing-observations.cpp
+++ 2155-find-missing-observations/find-missing-observations.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    int speedup = []{ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); return 0;}();
     vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
         int m = rolls.size();
         int rollsSum = 0;
