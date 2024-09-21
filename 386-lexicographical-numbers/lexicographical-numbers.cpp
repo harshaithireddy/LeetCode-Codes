332211716937d1802391ff0d7f23907c13c02bc4
Ithireddy Harsha Vardhan <119839919+harshaithireddy@users.noreply.github.com>
2024-09-21 10:27:09 +0530

    Time: 58 ms (28.79%) | Memory: 26.5 MB (20.13%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.cpp
+++ 386-lexicographical-numbers/lexicographical-numbers.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int speedup = []{ios::sync_with_stdio(0); cin.tie(0); return 0;}();
+    int speedup = []{ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); return 0;}();
 
     vector<int> lexicalOrder(int n) {
         vector<string> V;
