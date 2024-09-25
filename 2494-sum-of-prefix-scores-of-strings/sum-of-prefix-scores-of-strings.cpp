1a208b8b034bcfd613d42883028d53f6db0e4efd
Ithireddy Harsha Vardhan <119839919+harshaithireddy@users.noreply.github.com>
2024-09-25 20:03:19 +0530

    Time: 1257 ms (8.97%) | Memory: 733.3 MB (14.96%) - LeetSync

---
+++ 2494-sum-of-prefix-scores-of-strings/sum-of-prefix-scores-of-strings.cpp
@@ -0,0 +1,55 @@
+class TrieNode {
+public:
+    int count = 0;
+    unordered_map<char, TrieNode*> children;
+};
+
+class Trie {
+public:
+    TrieNode* root;
+    Trie() {
+        root = new TrieNode();
+    }
+
+    void insert(string word) {
+        TrieNode* node = root;
+        for(char c : word) {
+            if(!node->children[c]) {
+                node->children[c] = new TrieNode();
+            }
+            node = node->children[c];
+            node->count++;
+        }
+    }
+
+    int getPrefixScore(string prefix) {
+        TrieNode* node = root;
+        int score = 0;
+        for(char c : prefix) {
+            if(!node->children[c]) {
+                return 0;
+            }
+            node = node->children[c];
+            score += node->count;
+        }
+        return score;
+    }
+};
+
+class Solution {
+public:
+    vector<int> sumPrefixScores(vector<string>& words) {
+        Trie trie;
+        vector<int> res;
+
+        for(string word : words) {
+            trie.insert(word);
+        }
+
+        for(string word : words) {
+            int score = trie.getPrefixScore(word);
+            res.push_back(score);
+        }
+        return res;
+    }
+};
