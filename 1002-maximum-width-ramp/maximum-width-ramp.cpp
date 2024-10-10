02dee57584744769bca11cf14a18a83a25f6c18c
Ithireddy Harsha Vardhan <119839919+harshaithireddy@users.noreply.github.com>
2024-10-10 11:34:25 +0530

    Time: 26 ms (94.61%) | Memory: 47.3 MB (16.92%) - LeetSync

--- 1002-maximum-width-ramp/maximum-width-ramp.cpp
+++ 1002-maximum-width-ramp/maximum-width-ramp.cpp
@@ -1,8 +1,15 @@
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+
 class Solution {
 public:
     int maxWidthRamp(vector<int>& nums) {
         int n = nums.size();
-        
+
         vector<int> maxi(n);
         maxi[n-1] = nums[n-1];
         for(int i = n-2; i >= 0; i--) {
