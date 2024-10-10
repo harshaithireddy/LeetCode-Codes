16b90f0c0db2ddeaf9f91764494b127cd0daeac7
Ithireddy Harsha Vardhan <119839919+harshaithireddy@users.noreply.github.com>
2024-10-10 11:33:21 +0530

    Time: 47 ms (52.88%) | Memory: 47.4 MB (14.29%) - LeetSync

---
+++ 1002-maximum-width-ramp/maximum-width-ramp.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxWidthRamp(vector<int>& nums) {
+        int n = nums.size();
+        
+        vector<int> maxi(n);
+        maxi[n-1] = nums[n-1];
+        for(int i = n-2; i >= 0; i--) {
+            if(nums[i] >= maxi[i+1]) maxi[i] = nums[i];
+            else maxi[i] = maxi[i+1];
+        }
+
+        int res = 0;
+        int l = 0;
+        for(int r = 0; r < n; r++) {
+            while(nums[l] > maxi[r]) l++;
+            res = max(res, r - l);
+        }
+        return res;
+    }
+};
