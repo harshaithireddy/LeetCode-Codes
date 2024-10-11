ac576a790039b05dbf1e092a212fe7e74ccb2e2c
Ithireddy Harsha Vardhan <119839919+harshaithireddy@users.noreply.github.com>
2024-10-11 13:42:39 +0530

    Time: 116 ms (99.31%) | Memory: 67.9 MB (31.62%) - LeetSync

--- 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.cpp
+++ 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.cpp
@@ -1,3 +1,10 @@
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c++';
+}();
+
 class Solution {
 public:
     int smallestChair(vector<vector<int>>& times, int targetFriend) {
