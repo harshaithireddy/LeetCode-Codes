ac68de93ca68c04a686d63946f7dcfa1bfaf55be
Ithireddy Harsha Vardhan <119839919+harshaithireddy@users.noreply.github.com>
2024-10-12 12:24:07 +0530

    Time: 228 ms (93.47%) | Memory: 97 MB (67.79%) - LeetSync

---
+++ 2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minGroups(vector<vector<int>>& intervals) {
+        vector<int> start, end;
+        int n = intervals.size();
+        for(int i = 0; i < n; i++) {
+            start.push_back(intervals[i][0]);
+            end.push_back(intervals[i][1]);   
+        }
+        sort(start.begin(), start.end());
+        sort(end.begin(), end.end());
+
+        int j = 0;
+        int res = 0;
+        for(int i = 0; i < n; i++) {
+            if(start[i] > end[j]) j++;
+            else res++;
+        }
+        return res;
+    }
+};
