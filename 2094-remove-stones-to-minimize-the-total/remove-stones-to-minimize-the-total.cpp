9969a6482732734093c240f1f28509e5c3595282
Ithireddy Harsha Vardhan <119839919+harshaithireddy@users.noreply.github.com>
2024-10-14 14:53:52 +0530

    Time: 412 ms (71.66%) | Memory: 102.4 MB (96.03%) - LeetSync

--- 2094-remove-stones-to-minimize-the-total/remove-stones-to-minimize-the-total.cpp
+++ 2094-remove-stones-to-minimize-the-total/remove-stones-to-minimize-the-total.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int minStoneSum(vector<int>& piles, int k) {
+        ios::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);
+        
         priority_queue<int> pq(piles.begin(), piles.end());
 
         while(k--) {
