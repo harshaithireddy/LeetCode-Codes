5ca530206945dca83c1224a21375c945d8ae9bef
Ithireddy Harsha Vardhan <119839919+harshaithireddy@users.noreply.github.com>
2024-10-14 09:53:32 +0530

    Time: 187 ms (78.30%) | Memory: 79.8 MB (50.62%) - LeetSync

---
+++ 2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long maxKelements(vector<int>& nums, int k) {
+        priority_queue<int> pq;
+        for(int i = 0; i < nums.size(); i++) pq.push(nums[i]);
+
+        long long res = 0;
+        while(k--) {
+            int x = pq.top();
+            pq.pop();
+
+            res += x;
+            if(x % 3 == 0) {
+                pq.push(x / 3);
+            }
+            else pq.push(ceil(x / 3.0));
+        }
+        return res;
+    }
+};
