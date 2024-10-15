5cf6a6b20c92ab9c867667193874653a49cd278e
Ithireddy Harsha Vardhan <119839919+harshaithireddy@users.noreply.github.com>
2024-10-15 09:44:48 +0530

    Time: 22 ms (95.16%) | Memory: 15.8 MB (14.11%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
@@ -1,10 +1,17 @@
+auto speedUp = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c++';
+}();
+
 class Solution {
 public:
     long long minimumSteps(string s) {
         ios::sync_with_stdio(false);
         cin.tie(0);
         cout.tie(0);
-        
+
         long long cnt = 0, res = 0;
         for(int i = s.size() - 1; i >=0 ; i--) {
             if(s[i] == '0') cnt++;
