8902185470cff8ca23f85282f6254606489df025
Ithireddy Harsha Vardhan <119839919+harshaithireddy@users.noreply.github.com>
2024-10-20 10:32:55 +0530

    Time: 12 ms (34.13%) | Memory: 11 MB (44.10%) - LeetSync

---
+++ 1197-parsing-a-boolean-expression/parsing-a-boolean-expression.cpp
@@ -0,0 +1,53 @@
+class Solution {
+private:
+        char fun(vector<char>& V, char op) {
+        if(op == '&') {
+            for(char c : V) {
+                if(c == 'f') return 'f';
+            }
+            return 't';
+        }
+        else if(op == '|') {
+            for(char c : V) {
+                if(c == 't') return 't';
+            }
+            return 'f';
+        }
+        else if(op == '!') {
+            return (V[0] == 'f') ? 't' : 'f';
+        }
+        return 'f';
+    }
+public:
+    bool parseBoolExpr(string expression) {
+        vector<int> open;
+        int n = expression.size();
+
+        for(int i = 1; i < n; i++) {
+            if(expression[i] == '(') open.push_back(i);
+        }
+
+        for(int i = open.size() - 1; i >= 0; i--) {
+            vector<char> V;
+            int j = open[i];
+            char result;
+
+            while(j < n && expression[j] != ')') {
+                if(expression[j] == 'f' || expression[j] == 't') {
+                    V.push_back(expression[j]);
+                }
+                j++;
+            }
+            if(V.size() == 1) {
+                result = (expression[open[i] - 1] == '!') ? (V[0] == 'f' ? 't' : 'f') : V[0];
+            }
+            else {
+                result = fun(V, expression[open[i] - 1]);
+            }
+
+            expression.erase(expression.begin() + open[i] - 1, expression.begin() + j + 1);
+            expression.insert(expression.begin() + open[i] - 1, result);
+        }
+        return expression[0] == 't';
+    }
+};
