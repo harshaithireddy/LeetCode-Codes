1d7ab89ad11158f6723ca434d2adc2fd8b6b53b6
Ithireddy Harsha Vardhan <119839919+harshaithireddy@users.noreply.github.com>
2024-10-25 18:51:25 +0530

    Time: 50 ms (94.78%) | Memory: 45.7 MB (88.46%) - LeetSync

---
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<string> removeSubfolders(vector<string>& folder) {
+        sort(folder.begin(), folder.end());
+
+        vector<string> res;
+        res.push_back(folder[0]);
+        string temp = folder[0];
+        for(int i = 1; i < folder.size(); i++) {
+            if(folder[i].find(temp + "/") != 0) {
+                res.push_back(folder[i]);
+                temp=folder[i];
+            }
+        }
+        return res;
+    }
+};
