8d0ce1907c4eaf31f81dfddd52df17f422fe857c
Ithireddy Harsha Vardhan <119839919+harshaithireddy@users.noreply.github.com>
2024-10-26 23:30:30 +0530

    Time: 1073 ms (30.51%) | Memory: 309.9 MB (46.60%) - LeetSync

---
+++ 2545-height-of-binary-tree-after-subtree-removal-queries/height-of-binary-tree-after-subtree-removal-queries.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    map<int, pair<int, int>> nodeInfo;
+    map<int, priority_queue<pair<int, int>, vector<pair<int, int>>>> levelMap;
+
+    int computeHeight(TreeNode *root, int currentLevel) {
+        if(!root) return 0;
+        int leftHeight = 0, rightHeight = 0;
+        if(root->left) {
+            leftHeight = computeHeight(root->left, currentLevel + 1);
+        }
+        if(root->right) {
+            rightHeight = computeHeight(root->right, currentLevel + 1);
+        }
+        nodeInfo[root->val] = {max(leftHeight, rightHeight) + 1, currentLevel};
+        levelMap[currentLevel].push({max(leftHeight, rightHeight) + 1, root->val});
+        return max(leftHeight, rightHeight) + 1;
+    }
+
+    vector<int> treeQueries(TreeNode* root, vector<int>& queries) {
+        computeHeight(root, 0);
+        vector<int> res;
+        for(int q : queries) {
+            int level = nodeInfo[q].second;
+            int height = nodeInfo[q].first;
+            int maxHeight = 0;
+
+            if(levelMap[level].top().second != q) {
+                res.push_back(level + levelMap[level].top().first - 1);
+            }
+            else if(levelMap[level].size() == 1) res.push_back(level - 1);
+            else {
+                pair<int, int> topNode = levelMap[level].top();
+                levelMap[level].pop();
+                res.push_back(level + levelMap[level].top().first - 1);
+                levelMap[level].push(topNode);
+            }
+        }
+        return res;
+    }
+};
