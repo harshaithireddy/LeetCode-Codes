8eff3e7f981ccdeadfccb20e6bde200720d03b3f
Ithireddy Harsha Vardhan <119839919+harshaithireddy@users.noreply.github.com>
2024-10-27 19:10:30 +0530

    Time: 4 ms (69.14%) | Memory: 26.4 MB (92.75%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -1,3 +1,10 @@
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
 class Solution {
 public:
     int countSquares(vector<vector<int>>& matrix) {
