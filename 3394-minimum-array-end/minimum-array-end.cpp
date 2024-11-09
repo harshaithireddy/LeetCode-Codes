d8cb88f9a67f284ff68ed8575ff29ca15bafd0ac
Ithireddy Harsha Vardhan <119839919+harshaithireddy@users.noreply.github.com>
2024-11-09 19:59:51 +0530

    Time: 1770 ms (5.36%) | Memory: 8.6 MB (52.80%) - LeetSync

--- 3394-minimum-array-end/minimum-array-end.cpp
+++ 3394-minimum-array-end/minimum-array-end.cpp
@@ -1,13 +1,10 @@
-char init = []() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 'c';
-}();
-
 class Solution {
 public:
     long long minEnd(int n, int x) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+
         long long res = x;
         for(int i = 1; i < n; i++) {
             res = (res + 1) | x;
