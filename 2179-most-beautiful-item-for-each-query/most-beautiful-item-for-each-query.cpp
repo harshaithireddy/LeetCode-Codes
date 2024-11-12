e49ed2b388bfbe57571132803a62b01cb3cd8299
Ithireddy Harsha Vardhan <119839919+harshaithireddy@users.noreply.github.com>
2024-11-12 20:26:01 +0530

    Time: 53 ms (70.49%) | Memory: 94.1 MB (59.46%) - LeetSync

---
+++ 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
+        int n = items.size();
+        int m = queries.size();
+
+        vector<pair<int, int>> V;
+        for(int i = 0; i < m; i++) V.push_back({queries[i], i});
+
+        sort(V.begin(), V.end());
+        sort(items.begin(), items.end());
+
+        int maxi = 0, i = 0;
+        vector<int> res(m);
+        for(auto it : V) {
+            int price = it.first;
+            int ind = it.second;
+            while(i < n && items[i][0] <= price) {
+                maxi = max(maxi, items[i][1]);
+                i++;
+            }
+            res[ind] = maxi;
+        } 
+        return res;
+    }
+};
