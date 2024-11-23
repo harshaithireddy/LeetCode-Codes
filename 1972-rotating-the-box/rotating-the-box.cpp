391c774efb2ec94d30afd90f1295a72188418c2d
Ithireddy Harsha Vardhan <119839919+harshaithireddy@users.noreply.github.com>
2024-11-23 20:41:35 +0530

    Time: 204 ms (26.25%) | Memory: 60.9 MB (10.98%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.cpp
+++ 1972-rotating-the-box/rotating-the-box.cpp
@@ -1,50 +1,28 @@
 class Solution {
 public:
-    vector<vector<char>> rotateMatrix(vector<vector<char>>& matrix) {
-        ios_base::sync_with_stdio(false);
-        int m = matrix.size();
-        int n = matrix[0].size();
-
-        vector<vector<char>> transposed(n, vector<char>(m));
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                transposed[i][j] = matrix[j][i];
-            }
-        }
-
-        for(int i = 0; i < n; i++) {
-            reverse(transposed[i].begin(), transposed[i].end());
-        }
-        return transposed;
-    }
-
     vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
-        ios_base::sync_with_stdio(false);
-        for(auto &it : box) {
-            vector<char>& x = it;
-            int i = x.size() - 1;
-            int j = x.size() - 2;
-            while(j >= 0) {
-                if(x[j] == '*') {
-                    j--;
-                    i = j + 1;
-                }
-                else if(x[i] == '.' && x[j] == '#') {
-                    swap(x[i], x[j]);
+        int rows = box.size();
+        int cols = box[0].size();
+
+        for(int r = 0; r < rows; r++) {
+            int i = cols - 1;
+            for(int c = cols - 1; c >= 0; c--) {
+                if(box[r][c] == '#') {
+                    swap(box[r][c], box[r][i]);
                     i--;
-                    j--;
                 }
-                else if(x[i] == '.' && x[j] == '.') {
-                    j--;
-                }
-                else {
-                    i--;
-                    j--;
-                }
+                else if(box[r][c] == '*') i = c - 1;
             }
         }
 
-        vector<vector<char>> res = rotateMatrix(box);
+        vector<vector<char>> res;
+        for(int c = 0; c < cols; c++) {
+            vector<char> x;
+            for(int r = rows - 1; r >= 0; r--) {
+                x.push_back(box[r][c]);
+            }
+            res.push_back(x);
+        }
         return res;
     }
 };
