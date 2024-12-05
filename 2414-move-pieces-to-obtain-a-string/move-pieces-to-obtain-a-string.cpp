519823fc6e7246487e4a667cf64c18b5ebe846d7
Ithireddy Harsha Vardhan <119839919+harshaithireddy@users.noreply.github.com>
2024-12-05 17:14:49 +0530

    Time: 4 ms (90.23%) | Memory: 21.9 MB (35.59%) - LeetSync

---
+++ 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canChange(string start, string target) {
+        int s_id = 0, t_id = 0;
+        int n = start.size();
+
+        while(s_id < n || t_id < n) {
+            while(s_id < n && start[s_id] == '_') s_id++;
+            while(t_id < n && target[t_id] == '_') t_id++;
+
+            if(s_id == n && t_id == n) return true;
+            if(s_id == n || t_id == n) return false;
+
+            if((start[s_id] != target[t_id]) || (start[s_id] == 'L' && t_id > s_id) || (start[s_id] == 'R' && s_id > t_id)) return false;
+            
+            s_id++;
+            t_id++;
+        }
+        return true;
+    }
+};
