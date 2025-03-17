79a6eff0cfeb332904c8d3ce4ed1481161cf8b56
Ithireddy Harsha Vardhan <119839919+harshaithireddy@users.noreply.github.com>
2025-03-17 15:33:10 +0530

    Time: 19 ms (22.08%) | Memory: 60 MB (7.40%) - LeetSync

---
+++ 415-add-strings/add-strings.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        string s = "";
+        int i = num1.size() - 1, j = num2.size() - 1;
+        int carry = 0;
+        while(i >= 0 && j >= 0) {
+            int x = (num1[i] - '0') + (num2[j] - '0') + carry;
+            carry = x / 10;
+            x = x % 10;
+            s = to_string(x) + s;
+            i--;
+            j--;
+        }
+
+        while(i >= 0) {
+            int x = (num1[i] - '0') + carry;
+            carry = x / 10;
+            x = x % 10;
+            s = to_string(x) + s;
+            i--;
+        }
+
+        while(j >= 0) {
+            int x = (num2[j] - '0') + carry;
+            carry = x / 10;
+            x = x % 10;
+            s = to_string(x) + s;
+            j--;
+        }
+
+        if(carry) s = '1' + s;
+
+        return s;
+    }
+};
